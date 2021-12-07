64a4a41380692394012c1839d371a8479e339299
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-07 15:33:35 +0000

    Added datepicker

--- app/api_caller.py
+++ app/api_caller.py
@@ -8,7 +8,8 @@ default_params= {
 
 #url = https://api.nasa.gov/planetary/apod
 
-def get_image_url():
+def get_image_info(date="2021-12-07"):
+    default_params["date"] = date
     response = requests.get(f"https://api.nasa.gov/planetary/apod", params=default_params)
     response_json = response.json()
-    return response_json['url']
+    return response_json['url'], response_json['explanation']
--- app/app.py
+++ app/app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
-from app.api_caller import get_image_url
+from app.api_caller import get_image_info
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    image_url=get_image_url()
-    return render_template('index.html', landing_image=image_url)
+    image_url, image_description = get_image_info()
+    return render_template('index.html', landing_image=image_url, image_description=image_description)
 
 @app.route('/mars')
 def mars():
     return render_template('mars.html')
+
+@app.route("/request", methods=["POST"])
+def request_handler():
+    date = request.form.get("user_dateinput")
+    image_url, image_description = get_image_info(date)
+    return render_template('index.html', landing_image=image_url, image_description=image_description)
